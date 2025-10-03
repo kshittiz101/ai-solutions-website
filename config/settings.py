7c65f719f85fbc4c121a3e8823fc67f20b4ca74d
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-10-04 01:11:02 +0545

    fix clean code configuration is change

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,.vercel.app').sp
 # Application definition
 
 INSTALLED_APPS = [
+
     'jet.dashboard',
     'jet',
     'django.contrib.admin',
@@ -49,9 +50,6 @@ INSTALLED_APPS = [
 
     # external apps
     'base',
-
-
-
 
 ]
 
@@ -169,6 +167,43 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django JET Configuration
+JET_DEFAULT_THEME = 'default'
+JET_SIDE_MENU_COMPACT = True
+
+JET_THEMES = [
+    {
+        'theme': 'default',
+        'color': '#47bac1',
+        'title': 'Default'
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
